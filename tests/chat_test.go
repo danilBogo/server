2f63d9a22e8effeaaf028e1ac55a6a59ef61ad28
danilBogo <86552329+danilBogo@users.noreply.github.com>
2024-01-22 14:32:27 +0300

    update tests

--- tests/chat_test.go
+++ tests/chat_test.go
@@ -2,8 +2,13 @@ package tests
 
 import (
 	chatv1 "github.com/danilBogo/protos/gen/go/chat"
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"log/slog"
+	"os"
+	"server/internal/app"
+	"server/internal/config"
 	"server/tests/suite"
 	"testing"
 )
@@ -14,10 +19,21 @@ const (
 	chatId   = "chatId"
 )
 
+func init() {
+	cfg := config.MustLoad()
+
+	log := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+
+	application := app.New(log, cfg.GRPC.Port)
+
+	go application.GRPCServer.MustRun()
+}
+
 func TestJoin_HappyPath(t *testing.T) {
 	ctx, st := suite.New(t)
 
-	respJoin, err := st.ChatClient.Join(ctx, &chatv1.JoinRequest{Username: username})
+	currentUsername := username + uuid.New().String()
+	respJoin, err := st.ChatClient.Join(ctx, &chatv1.JoinRequest{Username: currentUsername})
 	require.NoError(t, err)
 	assert.NotEmpty(t, respJoin.ChatId)
 }
@@ -25,36 +41,39 @@ func TestJoin_HappyPath(t *testing.T) {
 func TestSend_HappyPath(t *testing.T) {
 	ctx, st := suite.New(t)
 
-	respJoin, err := st.ChatClient.Join(ctx, &chatv1.JoinRequest{Username: username})
+	currentUsername := username + uuid.New().String()
+	respJoin, err := st.ChatClient.Join(ctx, &chatv1.JoinRequest{Username: currentUsername})
 	require.NoError(t, err)
 	assert.NotEmpty(t, respJoin.ChatId)
 
-	_, err = st.ChatClient.Send(ctx, &chatv1.SendRequest{ChatId: respJoin.ChatId, Username: username, Text: text})
+	_, err = st.ChatClient.Send(ctx, &chatv1.SendRequest{ChatId: respJoin.ChatId, Username: currentUsername, Text: text})
 	require.NoError(t, err)
 }
 
 func TestLeave_HappyPath(t *testing.T) {
 	ctx, st := suite.New(t)
 
-	respJoin, err := st.ChatClient.Join(ctx, &chatv1.JoinRequest{Username: username})
+	currentUsername := username + uuid.New().String()
+	respJoin, err := st.ChatClient.Join(ctx, &chatv1.JoinRequest{Username: currentUsername})
 	require.NoError(t, err)
 	assert.NotEmpty(t, respJoin.ChatId)
 
-	_, err = st.ChatClient.Send(ctx, &chatv1.SendRequest{ChatId: respJoin.ChatId, Username: username, Text: text})
+	_, err = st.ChatClient.Send(ctx, &chatv1.SendRequest{ChatId: respJoin.ChatId, Username: currentUsername, Text: text})
 	require.NoError(t, err)
 
-	_, err = st.ChatClient.Leave(ctx, &chatv1.LeaveRequest{ChatId: respJoin.ChatId, Username: username})
+	_, err = st.ChatClient.Leave(ctx, &chatv1.LeaveRequest{ChatId: respJoin.ChatId, Username: currentUsername})
 	require.NoError(t, err)
 }
 
 func TestGetMessages_HappyPath(t *testing.T) {
 	ctx, st := suite.New(t)
 
-	respJoin, err := st.ChatClient.Join(ctx, &chatv1.JoinRequest{Username: username})
+	currentUsername := username + uuid.New().String()
+	respJoin, err := st.ChatClient.Join(ctx, &chatv1.JoinRequest{Username: currentUsername})
 	require.NoError(t, err)
 	assert.NotEmpty(t, respJoin.ChatId)
 
-	_, err = st.ChatClient.Send(ctx, &chatv1.SendRequest{ChatId: respJoin.ChatId, Username: username, Text: text})
+	_, err = st.ChatClient.Send(ctx, &chatv1.SendRequest{ChatId: respJoin.ChatId, Username: currentUsername, Text: text})
 	require.NoError(t, err)
 
 	c, err := st.ChatClient.GetMessages(ctx, &chatv1.GetMessagesRequest{ChatId: respJoin.ChatId})
@@ -63,10 +82,6 @@ func TestGetMessages_HappyPath(t *testing.T) {
 	r, err := c.Recv()
 	require.NoError(t, err)
 	assert.NotEmpty(t, r)
-
-	r, err = c.Recv()
-	require.Error(t, err)
-	assert.Empty(t, r)
 }
 
 func TestJoin_InvalidUsername(t *testing.T) {
@@ -79,29 +94,32 @@ func TestJoin_InvalidUsername(t *testing.T) {
 func TestJoin_AlreadyJoined(t *testing.T) {
 	ctx, st := suite.New(t)
 
-	respJoin, err := st.ChatClient.Join(ctx, &chatv1.JoinRequest{Username: username})
+	currentUsername := username + uuid.New().String()
+	respJoin, err := st.ChatClient.Join(ctx, &chatv1.JoinRequest{Username: currentUsername})
 	require.NoError(t, err)
 	assert.NotEmpty(t, respJoin.ChatId)
 
-	_, err = st.ChatClient.Join(ctx, &chatv1.JoinRequest{Username: username})
+	_, err = st.ChatClient.Join(ctx, &chatv1.JoinRequest{Username: currentUsername})
 	require.Error(t, err)
 }
 
 func TestSend_NotJoined(t *testing.T) {
 	ctx, st := suite.New(t)
 
-	_, err := st.ChatClient.Send(ctx, &chatv1.SendRequest{ChatId: chatId, Username: username, Text: text})
+	currentUsername := username + uuid.New().String()
+	_, err := st.ChatClient.Send(ctx, &chatv1.SendRequest{ChatId: chatId, Username: currentUsername, Text: text})
 	require.Error(t, err)
 }
 
 func TestSend_InvalidChatId(t *testing.T) {
 	ctx, st := suite.New(t)
 
-	respJoin, err := st.ChatClient.Join(ctx, &chatv1.JoinRequest{Username: username})
+	currentUsername := username + uuid.New().String()
+	respJoin, err := st.ChatClient.Join(ctx, &chatv1.JoinRequest{Username: currentUsername})
 	require.NoError(t, err)
 	assert.NotEmpty(t, respJoin.ChatId)
 
-	_, err = st.ChatClient.Send(ctx, &chatv1.SendRequest{ChatId: "", Username: username, Text: text})
+	_, err = st.ChatClient.Send(ctx, &chatv1.SendRequest{ChatId: "", Username: currentUsername, Text: text})
 	require.Error(t, err)
 }
 
@@ -119,25 +137,28 @@ func TestSend_InvalidUsername(t *testing.T) {
 func TestSend_InvalidText(t *testing.T) {
 	ctx, st := suite.New(t)
 
-	respJoin, err := st.ChatClient.Join(ctx, &chatv1.JoinRequest{Username: username})
+	currentUsername := username + uuid.New().String()
+	respJoin, err := st.ChatClient.Join(ctx, &chatv1.JoinRequest{Username: currentUsername})
 	require.NoError(t, err)
 	assert.NotEmpty(t, respJoin.ChatId)
 
-	_, err = st.ChatClient.Send(ctx, &chatv1.SendRequest{ChatId: respJoin.ChatId, Username: username, Text: ""})
+	_, err = st.ChatClient.Send(ctx, &chatv1.SendRequest{ChatId: respJoin.ChatId, Username: currentUsername, Text: ""})
 	require.Error(t, err)
 }
 
 func TestLeave_NotJoined(t *testing.T) {
 	ctx, st := suite.New(t)
 
-	_, err := st.ChatClient.Leave(ctx, &chatv1.LeaveRequest{ChatId: chatId, Username: username})
+	currentUsername := username + uuid.New().String()
+	_, err := st.ChatClient.Leave(ctx, &chatv1.LeaveRequest{ChatId: chatId, Username: currentUsername})
 	require.Error(t, err)
 }
 
 func TestLeave_InvalidChatId(t *testing.T) {
 	ctx, st := suite.New(t)
 
-	_, err := st.ChatClient.Leave(ctx, &chatv1.LeaveRequest{ChatId: "", Username: username})
+	currentUsername := username + uuid.New().String()
+	_, err := st.ChatClient.Leave(ctx, &chatv1.LeaveRequest{ChatId: "", Username: currentUsername})
 	require.Error(t, err)
 }
 
@@ -151,31 +172,36 @@ func TestLeave_InvalidUsername(t *testing.T) {
 func TestLeave_NotExistingChatId(t *testing.T) {
 	ctx, st := suite.New(t)
 
-	respJoin, err := st.ChatClient.Join(ctx, &chatv1.JoinRequest{Username: username})
+	currentUsername := username + uuid.New().String()
+	respJoin, err := st.ChatClient.Join(ctx, &chatv1.JoinRequest{Username: currentUsername})
 	require.NoError(t, err)
 	assert.NotEmpty(t, respJoin.ChatId)
 
-	_, err = st.ChatClient.Leave(ctx, &chatv1.LeaveRequest{ChatId: chatId, Username: username})
+	_, err = st.ChatClient.Leave(ctx, &chatv1.LeaveRequest{ChatId: chatId, Username: currentUsername})
 	require.Error(t, err)
 }
 
 func TestLeave_AfterLeave(t *testing.T) {
 	ctx, st := suite.New(t)
 
-	respJoin, err := st.ChatClient.Join(ctx, &chatv1.JoinRequest{Username: username})
+	currentUsername := username + uuid.New().String()
+	respJoin, err := st.ChatClient.Join(ctx, &chatv1.JoinRequest{Username: currentUsername})
 	require.NoError(t, err)
 	assert.NotEmpty(t, respJoin.ChatId)
 
-	_, err = st.ChatClient.Leave(ctx, &chatv1.LeaveRequest{ChatId: respJoin.ChatId, Username: username})
+	_, err = st.ChatClient.Leave(ctx, &chatv1.LeaveRequest{ChatId: respJoin.ChatId, Username: currentUsername})
 	require.NoError(t, err)
 
-	_, err = st.ChatClient.Leave(ctx, &chatv1.LeaveRequest{ChatId: respJoin.ChatId, Username: username})
+	_, err = st.ChatClient.Leave(ctx, &chatv1.LeaveRequest{ChatId: respJoin.ChatId, Username: currentUsername})
 	require.Error(t, err)
 }
 
 func TestGetMessages_InvalidChatId(t *testing.T) {
 	ctx, st := suite.New(t)
 
-	_, err := st.ChatClient.GetMessages(ctx, &chatv1.GetMessagesRequest{ChatId: chatId})
+	c, err := st.ChatClient.GetMessages(ctx, &chatv1.GetMessagesRequest{ChatId: chatId})
+	require.NoError(t, err)
+
+	_, err = c.Recv()
 	require.Error(t, err)
 }
